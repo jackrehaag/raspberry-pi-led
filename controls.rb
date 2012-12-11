6eb2e4b71769d39fe8baec8e478ad6f6c98d8d79
Jack Rehaag(Work iMac) <jackrehaag@hotmail.com>
2012-12-11 15:16:15 +0000

    added text messaging facility

--- controls.rb
+++ controls.rb
@@ -36,3 +36,27 @@ get '/setup' do
   @title = "Setup"
   erb :setup
 end
+
+get '/text_input' do
+  if @sms.content.include?('red')
+    Led.colour("red")
+    twiml = Twilio::TwiML::Response.new do |r|
+      twiml.Sms = "The colour has been set to red"
+    end
+  elsif @sms.content.include?('yellow')
+    Led.colour("yellow")
+    twiml = Twilio::TwiML::Response.new do |r|
+      twiml.Sms = "The colour has been set to yellow"
+    end
+  elsif @sms.content.include?('green')
+    Led.colour("green")
+    twiml = Twilio::TwiML::Response.new do |r|
+      twiml.Sms = "The colour has been set to green"
+    end
+  else
+    twiml = Twilio::TwiML::Response.new do |r|
+      twiml.Sms = "Sorry but you have not provided a correct colour, please choose from 'red', 'green' or 'yellow' "
+    end
+  end
+  twiml.text
+end
